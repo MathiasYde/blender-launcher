a492f9deef8a3a66625c0e91feb104a3165b480e
Mathias Yde <mathias.yde@hotmail.dk>
2023-11-24 23:24:19 +0100

    hello world egui program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use eframe::{App, Frame, NativeOptions, run_native};
+use egui::{CentralPanel, Context};
+
+struct Application;
+
+impl App for Application {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello World!");
+        });
+    }
+}
+
+impl Default for Application {
+			fn default() -> Self {
+				Self {}
+		}
+}
+
+impl Application {
+	fn new(cc: &eframe::CreationContext<'_>) -> Self {
+		Self
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+    run_native(
+			"Blender Launcher",
+			NativeOptions::default(),
+			Box::new(|cc| Box::new(Application::new(cc)))
+		).expect("Unable to initialize application");
 }
