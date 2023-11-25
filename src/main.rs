ac945e95b2723cf20cc5f20e2a474d4e25674e6a
Mathias Yde <mathias.yde@hotmail.dk>
2023-11-25 22:13:43 +0100

    basic yaml file saving
    finally figured out how to dump yaml :')

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::env;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 use eframe::{Frame, NativeOptions, run_native};
 use egui::{CentralPanel, Context, ScrollArea};
 use log;
 use std::process::Command;
 
 extern crate yaml_rust;
-use yaml_rust::{YamlLoader};
+use yaml_rust::{Yaml, YamlLoader};
 
 struct BlenderInstance {
 	name: String,
@@ -88,6 +88,33 @@ impl Application {
 				path: path.to_string(),
 			});
 		});
+	}
+
+	fn save_configuration(config_filepath: String, app: &Application) {
+		log::info!("Saving configuration to: {}", config_filepath);
+
+		let mut yaml_text = String::new(); {
+			let mut emitter = yaml_rust::YamlEmitter::new(&mut yaml_text);
+			let mut doc = yaml_rust::yaml::Hash::new();
+
+			let mut settings = yaml_rust::yaml::Hash::new();
+			settings.insert(Yaml::String("launch_on_system_startup".to_string()), Yaml::Boolean(false));
+
+			let mut instances = yaml_rust::yaml::Array::new();
+			app.instances.iter().for_each(|instance| {
+				let mut instance_doc = yaml_rust::yaml::Hash::new();
+				instance_doc.insert(Yaml::String("name".to_string()), Yaml::String(instance.name.clone()));
+				instance_doc.insert(Yaml::String("path".to_string()), Yaml::String(instance.path.clone()));
+				instances.push(Yaml::Hash(instance_doc));
+			});
+
+			doc.insert(Yaml::String("settings".to_string()), Yaml::Hash(settings));
+			doc.insert(Yaml::String("instances".to_string()), Yaml::Array(instances));
+			emitter.dump(&Yaml::Hash(doc)).unwrap();
+		}
+
+		let mut yaml_file = File::create(config_filepath).expect("Unable to create config file");
+		yaml_file.write_all(yaml_text.as_bytes()).expect("Unable to write config file");
 	}
 
 	fn launch_instance(&self, instance: &BlenderInstance) {
